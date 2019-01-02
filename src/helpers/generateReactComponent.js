b390d6730374dd3685e49216152540af17774431
Michal Kokocinski <michal@waat.eu>
2019-01-02 18:08:20 +0100

    Selecting react component type in generation process

--- src/helpers/generateReactComponent.js
+++ src/helpers/generateReactComponent.js
@@ -1,8 +1,10 @@
 import * as afs from 'async-file'
 import fs from 'fs-extra'
 import changeCase from 'case'
-import getFronthackPath from '../helpers/getFronthackPath'
+
+import getFronthackPath from './getFronthackPath'
 import consoleColors from './consoleColors'
+import userInput from './userInput'
 
 
 /**
@@ -27,9 +29,20 @@ export default async (projectRoot, isNext, type, machinename, description = null
       break;
 
     default:
-      reactComponentTemplatePath = isNext
-      ? `${fronthackPath}/templates/react-component-functional.js`
-      : `${fronthackPath}/templates/react-component-class.js`
+      if (!isNext) {
+        const { componentType } = await userInput({
+          name: 'componentType',
+          description: `Choose a type of component?\n1 - Stateless ( const ${machinename} = () => )\n2 - Class     ( class ${machinename} extends React.Component )\n`,
+          pattern: '1|2',
+          message: 'Choose option "1" or "2"',
+          type: 'number',
+        })
+        reactComponentTemplatePath = (componentType === 1)
+          ? `${fronthackPath}/templates/react-component-functional.js`
+          : `${fronthackPath}/templates/react-component-class.js`
+      } else {
+        reactComponentTemplatePath = `${fronthackPath}/templates/react-component-functional.js`
+      }
       break;
   }
 
