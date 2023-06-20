dc49132780f33de2a59944a0abe38014b8db8b53
Michal Kokocinski <michal.kokocinski@homeflow.co.uk>
2023-06-20 12:53:49 +0200

    Finish the updated Next.js configuration.

--- templates/next-repo/src/pages/_document.tsx
+++ templates/next-repo/src/pages/_document.tsx
@@ -10,7 +10,6 @@ export default function Document() {
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
       </Head>
       <body>
-        test
         <Main />
         <NextScript />
       </body>
